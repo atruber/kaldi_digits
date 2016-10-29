b5f51a90178247a6df38d70394a250c57c807c44
Adrianne Schwarzentruber <atruber@gmail.com>
2016-10-29 17:24:23 -0400

    a

--- data_prep.py
+++ data_prep.py
@@ -9,20 +9,26 @@ zeroes = []
 ones = []
 for fn in os.listdir('/home/u/fall15/atruber/tidigits/data/adults/train/man'):
     for f in os.listdir('/home/u/fall15/atruber/tidigits/data/adults/train/man/%s' % fn):
-        zeroes.append('train/man-{}_{}'.format(fn,f))
+        zeroes.append('train/man/{}/{}'.format(fn,f))
           # => training set
    # elif fn.startswith('1'):
         #ones.append(fn)     # => test set
 for fn in os.listdir('/home/u/fall15/atruber/tidigits/data/adults/test/man'):
     for f in os.listdir('/home/u/fall15/atruber/tidigits/data/adults/test/man/%s' % fn):
-        ones.append('test/man-{}_{}'.format(fn,f))
+        ones.append('test/man/{}/{}'.format(fn,f))   #set/gender/<spk id>/<transcription><letter>.wav
+
+def get_spk_id(filename):
+    return filename.split('/')[2]
+
+def get_utt_id(filename): #always preceeded by spkid for sorting
+    return get_spk_id(filename)+filename.split('/')[3]).split('.')[0] #<transcription><letter>
 
 def text(filenames):
     results = []
     for filename in filenames:
-        basename = filename.split('.')[0]
-        transcript = basename.split('_')[1][:-1]
-        results.append("{} {}".format(basename.split('.')[0], " ".join(transcript)))
+        basename = get_spk_id(filename) + '_' + get_utt_id(filename)  #<spkid>_<transcription><letter>
+        transcript = get_utt_id[:-1]
+        results.append("{} {}".format(basename, " ".join(transcript)))
     return '\n'.join(sorted(results))
 
 with open('data/train_digits/text', 'w') as train_text, open('data/test_digits/text', 'w') as test_text:
@@ -33,9 +39,10 @@ with open('data/train_digits/text', 'w') as train_text, open('data/test_digits/t
 def wav_scp(filenames):
     results= []
     for filename in filenames:
-        basename = filename.split('.')[0]
-        audio = basename sph2pipe -f wav ../tidigits/data/adults/filename.split('-')[0]/basename.split('-')[0]/basename.split('-')[1] |
-        results.append("{} {}".format(basename.split('.')[0], audio)
+        spkid = get_spk_id(filename)
+        basename = get_spk_id(filename) + '_' + get_utt_id(filename)  #<spkid>_<transcription><letter>
+        pipe = 'sph2pipe -f wav ' + '/home/u/fall15/atruber/tidigits/data/adults/' +filename + ' |'
+        results.append("{} {}".format(basename, pipe)
     return '\n'.join(sorted(results))
 
 with open('data/train_digits/wav.scp', 'w') as train_text, open('data/test_digits/wav.scp', 'w') as test_text:
@@ -47,9 +54,8 @@ with open('data/train_digits/wav.scp', 'w') as train_text, open('data/test_digit
 def utt2spk(filenames):
     results =[]
     for filename in filenames:
-        basename = filename.split('.')[0]
-        results.append("{} {}".format(basename.split('.')[0], "{} {}".format(basename.split('.')[0])))
-            return '\n'.join(sorted(results))
+        results.append("{} {}".format(get_utt_id(filename), get_spk_id(filename))
+    return '\n'.join(sorted(results))
 
 with open('data/train_digits/utt2spk', 'w') as train_text, open('data/test_digits/utt2spk', 'w') as test_text:
     train_text.write(utt2spk(zeroes))
