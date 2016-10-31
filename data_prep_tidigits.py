d08f49042ed473be9a7741ce06f2f228a804d4f3
Adrianne Schwarzentruber <atruber@gmail.com>
2016-10-30 23:05:05 -0400

    a

--- data_prep_tidigits.py
+++ data_prep_tidigits.py
@@ -30,7 +30,7 @@ def make_sets(training, test):
             for f in os.listdir(set +'/%s' % fn):
                 a = 'adults' if 'adults' in set else 'children'
                 s = 'train' if 'train' in set else 'test'
-                g = 'man' if 'man' in set else 'woman'
+                g = 'woman' if 'woman' in set else 'man'
                 ones.append(a + '/' + s + '/' + g + '/{}/{}'.format(fn,f))   #set/gender/<spk id>/<transcription><letter>.wav
 
 make_sets(training, test)
